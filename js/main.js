0cc5353e29525340db86b071b3fadd967e33d540
marianadiaz12 <mariana.diazes@gmail.com>
2016-11-24 16:58:08 -0600

    Primera parte modificado

--- js/main.js
+++ js/main.js
@@ -1,82 +1,83 @@
-var mexico =[{
-	name : "Carmen"
-	imagen : "carmen.jpg"
-	},
-	{
-	name : "Okaidy"
-	imagen : "okaidy.jpg"
-	},
-	{
-	name : "Orianna"
-	imagen : "orianna.jpg"
-	},
-	{
-	name : "Hass"
-	imagen : "hass.jpg"
-	},
-	{
-	name : "Nayeli"
-	imagen : "nayeli.jpg"
-	}];
-var chile =[{
-	name : "Amber"
-	imagen : "amber.jpg"
-	},
-	{
-	name : "Daniela"
-	imagen : "daniela.jpg"
-	},
-	{
-	name : "Euridiz"
-	imagen : "euridiz.jpg"
-	},
-	{
-	name : "Lia"
-	imagen : "lia.jpg"
-	},
-	{
-	name : "Yenay"
-	imagen : "yenay.jpg"
-	}];
-var arequipa =[{
-	name : "Anna"
-	imagen : "anna.jpg"
-	},
-	{
-	name : "Becky"
-	imagen : "becky.jpg"
-	},
-	{
-	name : "Brigette"
-	imagen : "brigette.jpg"
-	},
-	{
-	name : "Francia"
-	imagen : "francia.jpg"
-	}];
-var lima =[{
-	name : "Analucia"
-	imagen : "analucia.jpg"
-	},
-	{
-	name : "Lorena"
-	imagen : "lorena.jpg"
-	},
-	{
-	name : "Marilyn"
-	imagen : "marilyn.jpg"
-	},
-	{
-	name : "Zarai"
-	imagen : "zarai.jpg"
-	}];
+// var mexico =[{
+// 	name : "Carmen"
+// 	imagen : "carmen.jpg"
+// 	},
+// 	{
+// 	name : "Okaidy"
+// 	imagen : "okaidy.jpg"
+// 	},
+// 	{
+// 	name : "Orianna"
+// 	imagen : "orianna.jpg"
+// 	},
+// 	{
+// 	name : "Hass"
+// 	imagen : "hass.jpg"
+// 	},
+// 	{
+// 	name : "Nayeli"
+// 	imagen : "nayeli.jpg"
+// 	}];
+// var chile =[{
+// 	name : "Amber"
+// 	imagen : "amber.jpg"
+// 	},
+// 	{
+// 	name : "Daniela"
+// 	imagen : "daniela.jpg"
+// 	},
+// 	{
+// 	name : "Euridiz"
+// 	imagen : "euridiz.jpg"
+// 	},
+// 	{
+// 	name : "Lia"
+// 	imagen : "lia.jpg"
+// 	},
+// 	{
+// 	name : "Yenay"
+// 	imagen : "yenay.jpg"
+// 	}];
+// var arequipa =[{
+// 	name : "Anna"
+// 	imagen : "anna.jpg"
+// 	},
+// 	{
+// 	name : "Becky"
+// 	imagen : "becky.jpg"
+// 	},
+// 	{
+// 	name : "Brigette"
+// 	imagen : "brigette.jpg"
+// 	},
+// 	{
+// 	name : "Francia"
+// 	imagen : "francia.jpg"
+// 	}];
+// var lima =[{
+// 	name : "Analucia"
+// 	imagen : "analucia.jpg"
+// 	},
+// 	{
+// 	name : "Lorena"
+// 	imagen : "lorena.jpg"
+// 	},
+// 	{
+// 	name : "Marilyn"
+// 	imagen : "marilyn.jpg"
+// 	},
+// 	{
+// 	name : "Zarai"
+// 	imagen : "zarai.jpg"
+// 	}];
 
 $("#sede").change(function(){
 	if($('#sede option:selected').val()==0){
-		$("#foto").append(mexico[0].imagen);
+		$("#foto").append("<img src=img/mexico/carmen.jpg>");
 		$('#boton').click(function(){
-			if ($('#name').val()== name) {
+			if ($('#name').val()== "Carmen") {
 				$('#mensaje').text("Excelente");
+				$('#name').val(" ");
 			}
 			else{
 				$('#mensaje').text('Intentalo de nuevo');
